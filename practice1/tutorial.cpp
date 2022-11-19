827be0588d1f3e8e839ee099595a8986cdef99ad
dute <315843093@qq.com>
2022-11-19 10:25:15 +0800

    commit use extern library

---
+++ practice1/tutorial.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Tutorial.h"
+#ifdef USE_MYMATH
+#include "mysqrt.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stdout, "Usage: %s number\n", argv[0]);
+        return 1;
+    }
+
+    double value  = atof(argv[1]);
+
+#ifdef USE_MYMATH
+    printf("USE_MYMATH!\n");
+    double result = mysqrt(value);
+#else
+    printf("USE_STDMATH!\n");
+    double result = sqrt(value) ;
+#endif
+    fprintf(stdout, "The square root of %g is %g\n", value, result);
+
+    return 0;
+}
